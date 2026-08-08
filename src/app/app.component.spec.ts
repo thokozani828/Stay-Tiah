64653e735432a9716b28fd9587e4283e81ae9ef7
Thokozani <mickeythokozani828@gmail.com>
2026-08-08 09:47:38 +0100

    updating the main fale for the icons

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+/// <reference types="jasmine" />
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
